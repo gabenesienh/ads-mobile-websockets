13c9343fee8ade1a9d079c82082b7baec588dd14
gabenesienh <gabrielenes@ienh.com.br>
2026-07-13 11:50:59 -0300

    Criar estrutura de componentes /client/components
    Criar folha de estilos
    (WIP) Criar componente NavTopo
    Criar diretório /client/assets

---
+++ client/components/navTopo.js
@@ -0,0 +1,5 @@
+export const NavTopo = () => `
+  <div class="nav-topo-container">
+    <img class="nav-topo-logo" src='/assets/pollenta.png' alt='Pollenta'>
+  </div>
+`;
---
+++ client/js/componentes.js
@@ -0,0 +1,5 @@
+import { NavTopo } from '/components/navTopo.js';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  document.getElementById('_NavTopo').innerHTML = NavTopo();
+});
